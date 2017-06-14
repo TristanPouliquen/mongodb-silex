865df62dfa05d56cbaa79cdb2bc2c3cbaa8cb290
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2017-06-14 12:43:00 -0400

    test

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $app->get('/numbersForDate', function(Application $app, Request $request){
   $date = $request->query->get('date');
   $regex= "/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/";
   if (preg_match($regex, $date)){
-    $results = $app['mongodb']->selectDatabase('admin')->selectCollection('escuchas')->find(['fecha' => $date], ['numero' => 1]);
+    $results = $app['mongodb']->selectDatabase('admin')->selectCollection('escuchas')->find(['fecha' => $date], ['numero' => true]);
 
     return new JsonResponse($results);
   } else {
